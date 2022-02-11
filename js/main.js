892f7a11e751996265eb65544061427cf610b6de
dron901161 <andreimezhiritsky@yandex.ru>
2022-02-11 22:28:51 +0300

    create: added map in hotel section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.hotel-swiper', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-button--next',
+    prevEl: '.hotel-button--prev',
   },
 });

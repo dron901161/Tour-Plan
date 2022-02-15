0daaa2de5a342c24c57713afa45e120269595cb0
dron901161 <andreimezhiritsky@yandex.ru>
2022-02-15 21:29:09 +0300

    create: added reviewss section and slide

--- js/main.js
+++ js/main.js
@@ -13,14 +13,14 @@ var hotelSwiper = new Swiper('.hotel-swiper', {
     },
 });
 
-var mreviewsSwiper = new Swiper('.reviews-slider', {
+var reviewsSwiper = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-button--next',
-    prevEl: '.reviews-button--prev',
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
       enabled: true,
